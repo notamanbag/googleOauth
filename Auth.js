dd87b72fe7d8e471274b39e27caa99488bc6b957
Aman Bagrecha <52565521+notamanbag@users.noreply.github.com>
2021-05-18 19:09:41 +0530

    Update Auth.js

--- Auth.js
+++ Auth.js
@@ -2,7 +2,7 @@ const passport = require('passport')
 const  GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
 
-const GOOGLE_CLIENT_ID = '111451724374-5rf7805fuer76iu8jarm8v5up7t6vhvh.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = '111451724374-5rf7805fuer76iu8jarm8v5up7t6vhvh.apps.googleusercontent.com'; // These are obtained by going into foofle credential .
 const GOOGLE_CLIENT_SECRET = '_fgR4yFK6fyeb--wPT-_caYl'
 
 passport.use(new GoogleStrategy({
